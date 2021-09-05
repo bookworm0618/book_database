7a720093e35a2ec2706ffbdf9927eb252a910097
Michelle <Michellew94086@gmail.com>
2021-09-04 21:08:13 -0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+import models
+
+# main menu - add, seaarchc, analysis, exit, view
+
+# add books to thee database
+# edit books
+# delete books
+# searchc books
+# data cleaning
+# loop runs program
---
+++ models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///books.db', echo=False)
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+
+class Book(Base):
+    __tablename__ = 'books'
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String)
+    author = Column(String)
+    date_published = Column(String)
+    price = Column(Integer)
